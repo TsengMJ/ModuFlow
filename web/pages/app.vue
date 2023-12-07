fe26a0c79ac82e1671504f6719823e3fd56eae68
TsengMJ <tsngmj@gmail.com>
2023-12-07 16:10:42 +0800

    add new nuxt

---
+++ web/pages/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <AppDescription />
+    <AppRecord />
+
+    <AppCombinationRunDialog />
+    <AppAssetCollectionNFTDialog />
+    <AppRevolvingLendingNFTDialog />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.app {
+  margin: 60px 0;
+}
+</style>
